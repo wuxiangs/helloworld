bb84117f9b5666be9c1060374bf3379d8da47c4b
wuxiang <1325668250@qq.com>
2022-09-21 15:10:26 +0800

    主方法提交

--- src/main/java/com/aisino/helloworld/HelloworldApplication.java
+++ src/main/java/com/aisino/helloworld/HelloworldApplication.java
@@ -3,6 +3,13 @@ package com.aisino.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * helloworld应用程序
+ *
+ * @author wuxiang
+ * @date 2022/09/21
+ */
 @SpringBootApplication
 public class HelloworldApplication {
 
